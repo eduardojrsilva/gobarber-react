571a3ec0cb1d0f01adbbe533a8753ef6c300e6ac
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-03-22 10:18:28 -0300

    GoBarber creating error tooltips - Eduardo

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useRef } from 'react';
-import { FiArrowLeft, FiLogIn, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
