ded133e40972ca8ccff8a61d428bb814d4c4cd59
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-04-04 17:27:45 -0300

    GoBarber creating user authentication

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 
+import { AuthProvider } from './hooks/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+
     <GlobalStyle />
   </>
 )
