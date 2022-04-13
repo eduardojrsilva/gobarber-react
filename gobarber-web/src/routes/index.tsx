0d095af7ad1589f11216f24690a0c30b57636c0e
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-04-13 11:18:48 -0300

    GoBarber creating protected routes

--- gobarber-web/src/routes/index.tsx
+++ gobarber-web/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Routes, Route } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
@@ -9,15 +10,59 @@ import ResetPassword from '../pages/ResetPassword';
 import Profile from '../pages/Profile';
 import Dashboard from '../pages/Dashboard';
 
+import ProtectedRoute from './ProtectedRoute';
+
 const Router: React.FC = () => (
   <Routes>
-    <Route path="/" element={<SignIn/>} />
-    <Route path="/signup" element={<SignUp/>} />
-    <Route path="/forgot-password" element={<ForgotPassword/>} />
-    <Route path="/reset-password" element={<ResetPassword/>} />
+    <Route
+      path="/"
+      element={
+        <ProtectedRoute>
+          <SignIn/>
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/signup"
+      element={
+        <ProtectedRoute>
+          <SignUp/>
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/forgot-password"
+      element={
+        <ProtectedRoute>
+          <ForgotPassword/>
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/reset-password"
+      element={
+        <ProtectedRoute>
+          <ResetPassword/>
+        </ProtectedRoute>
+      }
+    />
 
-    <Route path="/profile" element={<Profile/>} />
-    <Route path="/dashboard" element={<Dashboard/>} />
+    <Route
+      path="/profile"
+      element={
+        <ProtectedRoute isPrivate>
+          <Profile/>
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/dashboard"
+      element={
+        <ProtectedRoute isPrivate>
+          <Dashboard/>
+        </ProtectedRoute>
+      }
+    />
   </Routes>
 );
 
